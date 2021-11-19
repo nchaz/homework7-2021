157f73379d8e87096e2cac8e8acffe10e3b9b9fe
nchaz <78763372+nchaz@users.noreply.github.com>
2021-11-18 20:57:12 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,16 +1,15 @@
-var video;
+var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
 });
 
 document.querySelector('#play').addEventListener('click', function() {
 	console.log("Play Video");
-	var vid = document.querySelector("#user");
+	video.play()
 	var vol = document.querySelector("#volume");
 	vol.innerHTML = video.vol * 100 + "%";
-	vid.play()
+	
 	// slider?
 });
 
@@ -37,7 +36,35 @@ document.querySelector("#skip").addEventListener('click', function() {
 	}
 	else {
 		console.log("Original location " + video.currentTime)
-		video.currentTime = 0
-		console.log("Going back to beginning")
+		video.currentTime += 15
+		console.log("New location " + video.currentTime)
 	}
+});
+
+document.querySelector("#mute").addEventListener('click', function() {
+	
+	var mute = document.getElementById("mute")
+	console.log("vol");
+	if (mute.HTML == "Mute") {
+		mute.innerHTML = "Mute";
+		video.muted = false;
+	}
+	else  {
+		mute.innerHTML = "Unmute";
+		video.muted = true
+	}
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = this.value / 100;
+	vol.innerHTML = this.value + "%";
+	console.log(video.volume);
 });
